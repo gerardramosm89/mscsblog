71dad62275052e927d5a29781b0d6de5b8824ad4
gramos <gerard.ramosm89@gmail.com>
2017-05-10 07:20:22 -0700

    changes

--- src/reducers/signin_reducer.js
+++ src/reducers/signin_reducer.js
@@ -2,7 +2,7 @@
 export default function(state = {}, action) {
   switch (action.type) {
     case 'SIGN_IN':
-      console.log(action.payload.data);
+      console.log("action.payload.data from the reducer is: ", action.payload.data);
       localStorage.setItem('token', action.payload.data.token);
       return action.payload.data;
     default:

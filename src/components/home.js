d0bf0bac10b12aad451cbef7e14c473686030bdd
gramos <gerard.ramosm89@gmail.com>
2017-05-15 14:41:44 -0700

    changes

--- src/components/home.js
+++ src/components/home.js
@@ -10,11 +10,20 @@ class Home extends Component {
       <div>
         <header className="home-header__wrapper">
           <div className="home-header__text-container">
-            <h1>MSCS Blog.</h1>
+            <h1>MLHQ Blog.</h1>
             <p className="home-header--subheading">
               Hooking you up with the latest in machine learning.
             </p>
+            <div className="btns__container">
+              <button className="team-trial-btn btn btn-lg">
+                Start a team trial
+              </button>
+              <button className="free-trial-btn btn btn-lg">
+                Start a free trial
+              </button>
+            </div>
           </div>
+
         </header>
         <section className="info-section">
           info section

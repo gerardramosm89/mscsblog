aa2f35e523bb503decb1754c2bf5ce323ee296a3
gramos <gerard.ramosm89@gmail.com>
2017-05-20 22:20:30 -0700

    changes

--- src/components/user_pages/Dashboard.js
+++ src/components/user_pages/Dashboard.js
@@ -6,27 +6,54 @@ import axios from 'axios';
 class Dashboard extends Component {
   constructor(props) {
     super(props);
-
+    this.state = {
+      blogs: []
+    };
   }
 
   componentDidMount() {
     console.log('this.props.token is: ', this.props.token);
+    axios.post('http://localhost:3050/api/queryblogs', { token: this.props.token.token })
+    .then(response => {
+      console.log('response from axios post is: ', response);
+      this.setState({
+        blogs: response.data.blogs
+      }, console.log("this.state.blogs is: ", response.data.blogs));
+    });
   }
 
   componentDidUpdate() {
     console.log("Dashboard component updated!");
   }
 
-  newPost(data) {
-    axios.post('http://localhost:3050/api/', data)
-      .then(response => {
-        console.log('response from axios post is: ', response);
-      });
+  newPost() {
+    console.log('hi');
+
   }
+  renderBlogs() {
+    if (!this.state.blogs) {
+      return <div>No posts loaded, have you written any posts?</div>
+    }
+    // if (this.state.blogs.length == 0) {
+    //   return <div>loading</div>
+    // }
+    return this.state.blogs.map(blog => {
+      return (
+        <div key={blog._id}>
+          <h1>{blog.title}</h1>
+          <p>{blog.content}</p>
+        </div>
+      );
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>Dashboard Component</h1>
+        <p>the token is: {this.props.token.token}</p>
+        <button onClick={this.newPost.bind(this)} className="btn btn-primary">Query Blogs</button>
+        {this.renderBlogs()}
       </div>
     );
   }
--- src/reducers/signin_reducer.js
+++ src/reducers/signin_reducer.js
@@ -1,20 +1,20 @@
 if (localStorage.getItem('token')) {
   var currentToken = localStorage.getItem('token');
-  var currentUser = localStorage.getItem('user');
+  // var currentUser = localStorage.getItem('user');
 } else {
   currentToken = null;
-  currentUser = null;
+  // currentUser = null;
 }
-export default function(state = { message: '', token: currentToken, user: currentUser}, action) {
+export default function(state = { message: '', token: currentToken}, action) {
   switch (action.type) {
     case 'SIGN_IN':
       console.log("action.payload.data from the reducer is: ", action.payload.data);
       localStorage.setItem('token', action.payload.data.token);
-      localStorage.setItem('user', action.payload.data.user);
+      // localStorage.setItem('user', action.payload.data.user);
       return action.payload.data;
     case 'SIGN_OUT':
       localStorage.removeItem('token');
-      localStorage.removeItem('user');
+      // localStorage.removeItem('user');
       console.log('action.payload from sign out is: ', action.payload);
       return action.payload;
     default:

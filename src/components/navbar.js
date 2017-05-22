a5650a1dc1baa0b8703aadec099227596ba0b87d
gramos <gerard.ramosm89@gmail.com>
2017-05-22 14:48:16 -0700

    changes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -44,9 +44,9 @@ class Navbar extends Component {
           <li className="nav-item">
             <Link className="nav-link" to="/imageupload">Upload File</Link>
           </li>
-          <li className="nav-item">
+          {/*<li className="nav-item">
             <Link className="nav-link" to="/blogs">Blogs</Link>
-          </li>
+          </li>*/}
           <li className="nav-item">
             <Link className="nav-link" to="/signup">Sign Up</Link>
           </li>

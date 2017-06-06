8c40934f4bdc6273ec981d06a8491ec2b0c0a3a7
Gerard Ramos <gerard.ramosm89@gmail.com>
2017-06-06 15:31:49 -0700

    changes

--- src/reducers/signin_reducer.js
+++ src/reducers/signin_reducer.js
@@ -9,11 +9,9 @@ export default function(state = { message: '', token: currentToken}, action) {
   switch (action.type) {
     case 'SIGN_IN':
       localStorage.setItem('token', action.payload.data.token);
-      // localStorage.setItem('user', action.payload.data.user);
       return action.payload.data;
     case 'SIGN_OUT':
       localStorage.removeItem('token');
-      // localStorage.removeItem('user');
       return action.payload;
     default:
       return state;

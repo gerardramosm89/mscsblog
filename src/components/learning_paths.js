83cecdeeab322a525b644377664b8442ffc7c798
Gerard Ramos <gerard.ramosm89@gmail.com>
2017-07-03 14:56:20 -0700

    changes

--- src/components/learning_paths.js
+++ src/components/learning_paths.js
@@ -5,10 +5,11 @@ import { fetchByLearningPath } from '../actions/index';
 class LearningPaths extends Component {
   constructor(props) {
     super(props);
-    this.topics = [
+        this.topics = [
       {
         title: 'Statistical Learning',
         subheading: 'Interesting in learning about Machine Learning? So are we, this is for the people who want to dive into deeper learning and data analysis',
+        path: 'statistical-learning',
         icon: '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="12.5rem" height="12.5rem" viewBox="0 0 62 65" version="1.1"> <!-- Generator: Sketch 40.1 (33804) - http://www.bohemiancoding.com/sketch --> <title>machine-learning</title> <desc>Created with Sketch.</desc> <defs/> <g id="techno" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="machine-learning" transform="translate(1.000000, 1.000000)" stroke-width="2"> <path d="M15,59 C19.4,59 23,55.4 23,51 L23,51" id="Shape" stroke="#082947"/> <path d="M45,59 C40.6,59 37,55.4 37,51 L37,51" id="Shape" stroke="#082947"/> <path d="M30,63 L46,63 L46,60.3 C46,59.6 45.3,59 44.5,59 L30,59 L15.5,59 C14.8,59 14,59.6 14,60.3 L14,63 L30,63 L30,63 Z" id="Shape" stroke="#082947"/> <path d="M29,46 L31,46" id="Shape" stroke="#082947"/> <path d="M57.5,9.3 C58.9,9.9 60,11.3 60,12.9 L60,47 C60,49.2 58.2,51 56,51 L4,51 C1.8,51 0,49.2 0,47 L0,13 C0,10.8 1.8,9 4,9 L15.8,9" id="Shape" stroke="#082947"/> <path d="M0,41 L60,41" id="Shape" stroke="#082947"/> <path d="M21.2,12.2 C21.1,11.8 21.1,11.3 21.1,10.9 C21.1,6.4 24.8,2.8 29.4,2.8 C30.2,2.8 32.6,0.4 38.8,1.1 C40.9,1.3 43.4,2.4 44.9,3.9 C45.2,3.9 45.5,3.8 45.8,3.8 C49.9,4 54,9 53.6,13.3 C54,14.3 54.2,15.3 54.2,16.5 C54.2,19.9 52.3,22.8 49.4,24.2 L49.4,24.2 C45.9,26 46,28.8 45.6,30.6 C45.4,31.3 44.8,32.4 43.4,32.4 C41.6,32.4 41.4,30.4 41.4,30.4 C41.1,27.4 38.3,25.7 35.7,25.7 C35.5,25.7 35.3,25.7 35.1,25.8 C30.9,27.4 26.8,24.7 25.7,23.1 C25.4,22.6 20.2,21.5 20.2,15.6 C20.2,14.4 20.6,13.2 21.2,12.2 L21.2,12.2 Z" id="Shape" stroke="#009AAD"/> <path d="M21.2,12.2 C21.2,12.2 23.4,19.5 31.9,16" id="Shape" stroke="#009AAD"/> <path d="M25.7,16.7 C25.7,16.7 29.4,16.4 29.4,13.1" id="Shape" stroke="#009AAD"/> <path d="M44.9,3.9 C44.9,3.9 39.6,4 38.3,10" id="Shape" stroke="#009AAD"/> <path d="M39.7,6.8 C39.7,6.8 40.4,8.5 42.4,9.6" id="Shape" stroke="#009AAD"/> <path d="M35.2,25.9 C35.2,25.9 42.2,24.7 44.5,16.5" id="Shape" stroke="#009AAD"/> <path d="M42.7,20.3 C42.7,20.3 42.4,18.7 41,17.3" id="Shape" stroke="#009AAD"/> <path d="M18.2,24.9 L13.9,29.2" id="Shape" stroke="#009AAD"/> <path d="M22,27 L22,29" id="Shape" stroke="#009AAD"/> <path d="M16,21 L14,21" id="Shape" stroke="#009AAD"/> </g> </g> </svg>'
       },
       {
@@ -25,13 +26,14 @@ class LearningPaths extends Component {
   }
   componentDidMount() {
     this.props.fetchByLearningPath();
+
   }
   componentDidUpdate() {
   }
   renderTopics() {
     return this.topics.map(topic => {
       return (
-        <div key={topic.title} className="col-4">
+        <div onClick={() => this.props.history.push(`/${topic.path}`)}key={topic.title} className="col-4">
           <div className="topic-card">
             <div className="topic-card--top">
               <div className="topic-icon">

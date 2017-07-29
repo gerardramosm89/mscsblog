1b5bc04f38cf6ea600318a021d34a1ba83bef7b6
Gerard Ramos <gerard.ramosm89@gmail.com>
2017-07-28 21:37:23 -0700

    slight fix

--- src/components/navbar2.js
+++ src/components/navbar2.js
@@ -40,7 +40,7 @@ class Navbar2 extends Component {
             <Link className="nav-link" to="/learningpaths">Learning Paths<span className="sr-only">(current)</span></Link>
           </li>
         </ul>
-        { false ? '': (<Button color="primary" onClick={this.props.toggleModal}>Sign In</Button>) }
+        { this.props.token ? '': (<Button color="primary" onClick={this.props.toggleModal}>Sign In</Button>) }
         {this.props.token ? (
           <div className="dropdown" style={{color: 'white', cursor: 'pointer'}}>
             <a className="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">

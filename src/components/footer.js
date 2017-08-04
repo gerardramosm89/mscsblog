4983908ae802988cf4d2a2f93288a09122a0b761
Gerard Ramos <gerard.ramosm89@gmail.com>
2017-08-03 17:41:30 -0700

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 export default class Footer extends Component {
   render() {
     return(
-      <footer>
+      <footer style={{ background: '#292b2c'}}>
         <div className="text-center container-fluid">
-          <p className="footer-text">MLHQ.io | Join us on slack!</p>
+          <p className="footer-text" style={{color: 'white', marginBottom: '0', padding: '.7rem'}}>MLHQ.io | Join us on slack!</p>
         </div>
       </footer>
     );

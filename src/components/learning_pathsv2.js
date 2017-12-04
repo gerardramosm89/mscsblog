f4848d28fbfcc64138cc4faaa3453c5b3453d113
gerardramosayasdi <gerard.ramosm89@gmail.com>
2017-12-03 17:58:53 -0800

    cards progress

--- src/components/learning_pathsv2.js
+++ src/components/learning_pathsv2.js
@@ -1,14 +1,60 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class LearningPaths extends Component {
+  constructor(props) {
+    super(props);
+    const paths = [
+      {
+        title: 'Statistical Learning',
+        subheading: 'Interesting in learning about Machine Learning? So are we, this is for the people who want to dive into deeper learning and data analysis',
+        path: 'statistical-learning',
+        image: 'http://mlhq.io/img/pic02.jpg'
+      }
+    ];
+    this.state = {
+      paths
+    }
+  }
+  renderCards() {
+    return this.state.paths.map(path => {
+      return(
+        <div className="card-container">
+          <div className="card-header-image">
+            <img src="http://mlhq.io/img/pic02.jpg" />
+          </div>
+          <div className="card-text-learning">
+            <h1>Lorem Ipsum</h1>
+            <p>Interdum amet accumsan placerat commodo ut amet aliquam blandit nunc tempor lobortis nunc non. Mi accumsan.</p>
+          </div>
+          <div className="card-button">
+            <Link to="statistical-learning">Let me learn this</Link>
+          </div>
+        </div>
+      );
+    })
+  }
   render() {
     return(
       <div className="paths-container">
         <div className="card-container">
           <div className="card-header-image">
+            <img src="http://mlhq.io/img/pic02.jpg" />
+          </div>
+          <div className="card-text-learning">
+            <h1>Lorem Ipsum</h1>
+            <p>Interdum amet accumsan placerat commodo ut amet aliquam blandit nunc tempor lobortis nunc non. Mi accumsan.</p>
+          </div>
+          <div className="card-button">
+            <Link className="learning-path-button" to="statistical-learning">Let me learn this</Link>
+          </div>
+        </div>
+
+        <div className="card-container">
+          <div className="card-header-image">
           placeholder
           </div>
-          <div className="card-text">
+          <div className="card-text-learning">
             <h1>Lorem Ipsum</h1>
             <p>Interdum amet accumsan placerat commodo ut amet aliquam blandit nunc tempor lobortis nunc non. Mi accumsan.</p>
           </div>
@@ -21,7 +67,7 @@ export default class LearningPaths extends Component {
           <div className="card-header-image">
           placeholder
           </div>
-          <div className="card-text">
+          <div className="card-text-learning">
             <h1>Lorem Ipsum</h1>
             <p>Interdum amet accumsan placerat commodo ut amet aliquam blandit nunc tempor lobortis nunc non. Mi accumsan.</p>
           </div>
@@ -34,7 +80,7 @@ export default class LearningPaths extends Component {
           <div className="card-header-image">
           placeholder
           </div>
-          <div className="card-text">
+          <div className="card-text-learning">
             <h1>Lorem Ipsum</h1>
             <p>Interdum amet accumsan placerat commodo ut amet aliquam blandit nunc tempor lobortis nunc non. Mi accumsan.</p>
           </div>
@@ -47,20 +93,7 @@ export default class LearningPaths extends Component {
           <div className="card-header-image">
           placeholder
           </div>
-          <div className="card-text">
-            <h1>Lorem Ipsum</h1>
-            <p>Interdum amet accumsan placerat commodo ut amet aliquam blandit nunc tempor lobortis nunc non. Mi accumsan.</p>
-          </div>
-          <div className="card-button">
-            <button>Click Me</button>
-          </div>
-        </div>
-
-        <div className="card-container">
-          <div className="card-header-image">
-          placeholder
-          </div>
-          <div className="card-text">
+          <div className="card-text-learning">
             <h1>Lorem Ipsum</h1>
             <p>Interdum amet accumsan placerat commodo ut amet aliquam blandit nunc tempor lobortis nunc non. Mi accumsan.</p>
           </div>
